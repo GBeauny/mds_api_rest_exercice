9fa1c7f017cd6af6521fe7429aba087e003b452c
Geoffrey <geoffrey.beauny@publicissapient.com>
2020-04-20 14:07:45 +0200

    Update files

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-app.use(bodyParser.json()); // application/json
+app.use(bodyParser.json()); // content-type: application/json
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,3 +18,4 @@ app.use((req, res, next) => {
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
+
--- routes/feed.js
+++ routes/feed.js
@@ -1,7 +1,5 @@
 const express = require('express');
-
 const feedController = require('../controllers/feed');
-
 const router = express.Router();
 
 // GET /feed/posts
